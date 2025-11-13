e1de631f3f8974e8292cb3c34770609504fe3181
gerald-rocher <rocher.gerald@gmx.fr>
2025-11-13 16:13:01 +0100

    Added crazyflie_ros2_multiranger and ros_gz_crazyflie directories

---
+++ crazyflie_ros2_multiranger/crazyflie_ros2_multiranger_bringup/launch/simple_mapper_simulation.launch.py
@@ -0,0 +1,84 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.conditions import IfCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, Command
+
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    # Configure ROS nodes for launch
+
+    # Setup project paths
+    pkg_project_crazyflie_gazebo = get_package_share_directory('ros_gz_crazyflie_bringup')
+    pkg_crazyflie_description = get_package_share_directory('crazyflie_description')
+
+    # Setup to launch a crazyflie gazebo simulation from the ros_gz_crazyflie project
+    crazyflie_simulation = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_project_crazyflie_gazebo, 'launch', 'crazyflie_simulation.launch.py'))
+    )
+
+
+    # Load URDF from XACRO file
+    xacro_file = os.path.join(pkg_crazyflie_description, 'urdf', 'crazyflie_body.xacro')
+    joints_config = os.path.join(pkg_project_crazyflie_gazebo, 'config', 'joints.yaml')
+ 
+    robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        output='screen',
+        parameters=[{
+            'robot_description': Command(['xacro ', xacro_file]),
+            'use_sim_time': True
+        }]
+    )
+    
+    joint_state_publisher = Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        name='joint_state_publisher',
+        parameters=[joints_config] if os.path.exists(joints_config) else [{'use_sim_time': True}]
+    )
+    
+    simple_mapper = Node(
+        package='crazyflie_ros2_multiranger_simple_mapper',
+        executable='simple_mapper_multiranger',
+        name='simple_mapper',
+        output='screen',
+        parameters=[
+            {'robot_prefix': 'crazyflie'},
+            {'use_sim_time': True}
+        ]
+    )
+
+    rviz_config_path = os.path.join(
+        get_package_share_directory('crazyflie_ros2_multiranger_bringup'),
+        'config',
+        'sim_mapping.rviz')
+
+    rviz = Node(
+            package='rviz2',
+            namespace='',
+            executable='rviz2',
+            name='rviz2',
+            arguments=['-d', rviz_config_path],
+            parameters=[{
+                "use_sim_time": True
+            }]
+            )
+
+    return LaunchDescription([
+        robot_state_publisher,
+        joint_state_publisher,
+        crazyflie_simulation,
+        simple_mapper,
+        rviz
+        ])
